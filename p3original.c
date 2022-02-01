f12c593fed9384d1e073699e6a05e8a575c8cb19
bhanuteja199 <94587545+Bhanuteja199@users.noreply.github.com>
2022-02-01 10:05:13 +0000

    p3 completed

--- p3original.c
+++ p3original.c
@@ -2,32 +2,29 @@
 int input()
 {
   int n;
-  printf("enter the value of n\n");
+  printf("enter value of n\n");
   scanf("%d",&n);
-
+  return n;
 }
 int add(int n)
 {
   int sum=0,i;
+
   for(i=1;i<=n;i++)
   {
     sum=sum+i;
-
   }
   return sum;
 }
 int output(int sum)
 {
   printf("sum is %d",sum);
-
 }
 int main()
 {
-  int n,sum;
+  int n, sum;
   n=input();
   sum=add(n);
   output(sum);
   return 0;
 }
-
-
